3b52f023a8bdf41ceb9833eb030040705663010b
Ruslan Rakshinsky <ruslanfox.eerror@outlook.com>
2023-01-14 14:51:27 +0300

    kind of implemented input

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Employee {
+    private final String name;
+    private final int workingHours;
+    private final float rate;
+    private float salary;
+    private final String personalNumber;
+
+    public Employee(String name, int workingHours, float rate, String personalNumber) {
+        this.name = name;
+        this.workingHours = workingHours;
+        this.rate = rate;
+        this.personalNumber = personalNumber;
+    }
+
+    public String getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWorkingHours() {
+        return workingHours;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
 }
--- src/main/java/org/example/Employees.java
+++ src/main/java/org/example/Employees.java
@@ -1,15 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Employees implements IEmployeeInfo {
+    List<Employee> employeeList = new ArrayList<>();
 
     @Override
     public void Input() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            for (int i = 0; i < n; i++){
+                employeeList.add(new Employee(scanner.nextLine(), scanner.nextInt(), scanner.nextFloat(), scanner.nextLine()));
+            }
 
+
+        } catch (NumberFormatException exception) {
+
+        }
     }
 
     @Override
     public void Display() {
-
     }
 
     @Override
